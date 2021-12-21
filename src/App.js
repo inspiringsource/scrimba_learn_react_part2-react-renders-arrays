33c15f7984e33557bd1e70747b87a4e0080ba2ff
inspiringsource <ab.b2k3@gmail.com>
2021-12-21 12:15:26 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
+  const colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+          
+      </div>
+  )
 }
 
 export default App;
+            // <Joke 
+            //     punchline="It’s hard to explain puns to kleptomaniacs because they always take things literally."
+            // />
+            // <Joke 
+            //     setup="I got my daughter a fridge for her birthday." 
+            //     punchline="I can't wait to see her face light up when she opens it." 
+            //     isPun={false}
+            // />
+            // <Joke 
+            //     setup="How did the hacker escape the police?" 
+            //     punchline="He just ransomware!"
+            //     isPun={true}
+            // />
+            // <Joke 
+            //     setup="Why don't pirates travel on mountain roads?" 
+            //     punchline="Scurvy." 
+            //     isPun={true}
+            // />
+            // <Joke 
+            //     setup="Why do bees stay in the hive in the winter?" 
+            //     punchline="Swarm." 
+            //     isPun={true}
+            // />
+            // <Joke 
+            //     setup="What's the best thing about Switzerland?" 
+            //     punchline="I don't know, but the flag is a big plus!" 
+            //     isPun={false}
+            // />
